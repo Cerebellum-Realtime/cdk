40402ca23a07bc4363f2634f0269e2475d0ac10c
Will Conrad <w.conrad@outlook.com>
2024-07-02 13:27:49 -0500

    Build: Elasticache in it's own module, added connection string
    Co-authored-by: Dylan Spyer <115838205+dylanspyer@users.noreply.github.com>

--- lib/websocket-server-ecs-stack.ts
+++ lib/websocket-server-ecs-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
+import { Elasticache } from "./Elasticache";
 import * as ecs from "aws-cdk-lib/aws-ecs";
 import * as ec2 from "aws-cdk-lib/aws-ec2";
 
@@ -7,12 +8,11 @@ export class WebsocketServerEcsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    //creates the virtual private networkv
     const vpc = new ec2.Vpc(this, "MyVpc", { maxAzs: 2 });
-    //create the cluster where we will deploy our container, called MyCluster
-    //uses the vpc that we defined above
     const cluster = new ecs.Cluster(this, "MyCluster", { vpc });
-    //creates a taskDefinition
+
+    const elasticache = new Elasticache(scope, id, vpc);
+
     const taskDefinition = new ecs.FargateTaskDefinition(
       this,
       "websocket-task-file",
@@ -22,28 +22,42 @@ export class WebsocketServerEcsStack extends cdk.Stack {
       }
     );
 
-    //We are adding the container to the taskDefinition, and information
+    const serviceSecurityGroup = new ec2.SecurityGroup(
+      this,
+      "serviceSecurityGroups",
+      {
+        vpc,
+        securityGroupName: "fargate-service-sec-group",
+      }
+    );
+
+    serviceSecurityGroup.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(8000),
+      "Allow access to port 8000 from any IP"
+    );
+
     taskDefinition.addContainer("MyContainer", {
       image: ecs.ContainerImage.fromRegistry(
         "public.ecr.aws/q8e0a8z0/websocket-server:latest"
-      ), //works because it is public.
+      ),
       memoryLimitMiB: 2048,
       cpu: 1024,
       portMappings: [{ containerPort: 8000 }],
+      environment: {
+        REDIS_ENDPOINT_ADDRESS: elasticache.redisEndpointAddress,
+        REDIS_ENDPOINT_PORT: elasticache.redisEndpointPort
+      },
     });
 
     const service = new ecs.FargateService(this, "WebsocketFargateService", {
       cluster,
       taskDefinition,
       desiredCount: 1,
-      assignPublicIp: true, //Give this badboy a public ip address
+      assignPublicIp: true,
+      securityGroups: [serviceSecurityGroup],
     });
-    //This allows us to access the container directly from the internet
-    const securityGroup = service.connections.securityGroups[0];
-    securityGroup.addIngressRule(
-      ec2.Peer.anyIpv4(),
-      ec2.Port.tcp(8000),
-      "Allow access to port 8000 from any IP"
-    );
+
+    service.node.addDependency(elasticache);
   }
 }
